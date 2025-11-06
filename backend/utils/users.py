d4e5c5ab5ce7d4a16b83f784143a86c6aa4f0486
CodingWithSaksham <sakshamthecoder@gmail.com>
2025-11-06 22:42:38 +0530

    Fixed bugs

--- backend/utils/users.py
+++ backend/utils/users.py
@@ -2,15 +2,17 @@ from typing import Annotated
 
 from fastapi import Depends
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.core.middleware import get_current_user
 from backend.schemas.users.permissions import Permissions
-from data.database import DBSession
+
 from data.models.users import User
+from data.database import get_db
 
 
 async def get_permissions(
-    user=Depends(get_current_user), session=DBSession
+    user=Depends(get_current_user), session: AsyncSession = Depends(get_db)
 ) -> Permissions:
     stml = select(User.permissions).where(User.username == user["username"])
 
