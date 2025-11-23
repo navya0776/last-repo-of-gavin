77d4286b5a5f91fed50fa7496f7ac247708c42ec
Ashwin-garg <ashwingarg202106@gmail.com>
2025-11-23 12:38:36 +0530

    Testing (only for abhi sir)

---
+++ scripts/store_migrations.sql
@@ -0,0 +1,10 @@
+INSERT INTO stores (store_id, store_name) VALUES
+(1, 'ENGR STORE'),
+(2, 'ORDNANCE STORE'),
+(3, 'KRAZ STORE'),
+(4, 'EXP STORE'),
+(5, 'INDG STORE'),
+(6, 'SCANIA STORE'),
+(7, 'TATRA STORE'),
+(8, 'TATA TIPPER'),
+(9, 'ARV STORE');
